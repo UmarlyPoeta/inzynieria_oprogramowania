be4171ec91db19f583a993ebe2e628f6bff6743b
Perktus <mefixek2019@gmail.com>
2025-10-12 18:54:04 +0200

    chore(frontend): setup initial folder structure and config

---
+++ project/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { ReactLogo } from '@/assets';
+
+const App = () => {
+  return (
+    <>
+      <div >
+        <a href="https://react.dev" target="_blank">
+          <img src={ReactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>NetSimCPP</h1>
+    </>
+  )
+}
+
+export default App
---
+++ project/frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  resolve: { 
+    alias: { 
+      '@/components': resolve(__dirname, 'src/components/index.ts'),
+      '@/context': resolve(__dirname, 'src/context/index.ts'),
+      '@/data': resolve(__dirname, 'src/data/index.ts'),
+      '@/features': resolve(__dirname, 'src/features/index.ts'),
+      '@/hooks': resolve(__dirname, 'src/hooks/index.ts'),
+      '@/pages': resolve(__dirname, 'src/pages/index.ts'),
+      '@/utils': resolve(__dirname, 'src/utils/index.ts'),
+      '@/assets': resolve(__dirname, 'src/assets/index.ts'),
+      '@/api': resolve(__dirname, 'src/api/index.ts')
+    },
+  },
+  plugins: [react()],
+})
