be2693d30b1ca0efa2b9c8b357acbe7622d43086
Perktus <mefixek2019@gmail.com>
2025-10-27 00:34:36 +0100

    refactor: add centralized exports for editor components

--- project/frontend/src/components/index.ts
+++ project/frontend/src/components/index.ts
@@ -4,3 +4,6 @@ export { default as RightSidebar } from './layout/RightSidebar/RightSidebar';
 export { default as CanvasArea } from './layout/CanvasArea/CanvasArea';
 
 export { default as DeviceNode } from './editor/DeviceNode/DeviceNode';
+export { default as GroupNode } from './editor/ContextGroupMenu/ContextGroupMenu';
+export { default as GlobalContextMenu } from './editor/GlobalContextMenu/GlobalContextMenu';
+export { default as ContextGroupMenu } from './editor/ContextGroupMenu/ContextGroupMenu';
