65fd51f113e0bcfe16f1f151ab7d21574114e23a
Perktus <perktus@dev.pl>
2025-10-28 18:29:40 +0100

    chore: remove Sidebar component in favor of RightSidebar and LeftSidebar

--- project/frontend/src/components/index.ts
+++ project/frontend/src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Sidebar } from './layout/Sidebar/Sidebar';
 export { default as LeftSidebar } from './layout/LeftSidebar/LeftSidebar';
 export { default as RightSidebar } from './layout/RightSidebar/RightSidebar';
 export { default as CanvasArea } from './layout/CanvasArea/CanvasArea';
