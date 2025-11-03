822c50575378d89b6e97b5a360561ebbbb9b4fc2
Perktus <mefixek2019@gmail.com>
2025-11-03 20:29:24 +0100

    fix: add missing export

--- project/frontend/src/hooks/index.ts
+++ project/frontend/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useFetch } from './useFetch/useFetch';
 export { default as useMutation } from './useMutation/useMutation';
+export { default as useAuth } from './useAuth/useAuth';
