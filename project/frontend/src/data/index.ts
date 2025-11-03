2a9d0ad494cd0bc4baf0856cab80ea7f996d446e
Perktus <mefixek2019@gmail.com>
2025-11-04 00:42:26 +0100

    feat: add exports for computer, router, and switch SVG icons

---
+++ project/frontend/src/data/index.ts
@@ -0,0 +1,3 @@
+export { default as ComputerIcon } from '@/data/svg/computer.svg'
+export { default as RouterIcon } from '@/data/svg/router.svg'
+export { default as SwitchIcon } from '@/data/svg/switch.svg'
