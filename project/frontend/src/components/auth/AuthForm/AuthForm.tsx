ce974baf23474fa259cf5eb061ae600df8b3d584
Perktus <mefixek2019@gmail.com>
2025-11-04 19:43:15 +0100

    feat: add validateData function

--- project/frontend/src/components/auth/AuthForm/AuthForm.tsx
+++ project/frontend/src/components/auth/AuthForm/AuthForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAuthContext } from '../../../context/AuthContext'; 
 import { useNavigate } from "react-router-dom";
+import { GoogleIcon } from '@/data';
 import { 
   Button, 
   FormContainer, 
@@ -25,11 +26,62 @@ const AuthForm = ({ mode }: AuthFormProps) => {
     const {login, register} = useAuthContext();
     const navigate = useNavigate(); 
 
+    const validateData = () => {
+      if (form.password !== form.confirmPassword) {
+          setError("Error! Passwords do not match.");
+          return false;
+      }
+
+      const passwordLength = form.password.length;
+      if (passwordLength < 6 || passwordLength > 15) {
+          setError("Error! Password must be between 6 and 15 characters long.");
+          return false;
+      }
+
+      const usernameLength = form.username.length;
+      if (usernameLength < 4 || usernameLength > 12) {
+          setError("Error! Username must be between 4 and 12 characters long.");
+          return false;
+      }
+
+      if (!/^[a-zA-Z0-9_]+$/.test(form.username)) {
+          setError("Error! Username can only contain letters, numbers, and underscores.");
+          return false;
+      }
+
+      if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,15}$/.test(form.password)) {
+          setError("Error! Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.");
+          return false;
+      }
+
+      if (/\s/.test(form.password)) {
+          setError("Error! Password cannot contain spaces.");
+          return false;
+      }
+
+      if (/\s/.test(form.username)) {
+          setError("Error! Username cannot contain spaces.");
+          return false;
+      }
+
+      if (form.password === form.username) {
+          setError("Error! Password cannot be the same as username.");
+          return false;
+      }
+
+      return true;
+    };
+
+
+
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
             if (mode === "login") await login(form);
-            else await register(form);
+            else {
+              if (!validateData()) return;
+              await register(form);
+            }
             navigate("/workspace");
         } catch (err: any) {
             setError(err.message || "Wystąpił błąd.");
@@ -60,8 +112,17 @@ const AuthForm = ({ mode }: AuthFormProps) => {
             <Button type="submit" color="#FC5D08" style={{marginTop: '.3rem'}}>
               {mode === "login" ? "Sign In" : "Sign Up"}
             </Button>
-            <Button type="button" color="#f3f3f3" style={{marginTop: '0.5rem'}} fontColor="#121212">
-              {mode === "login" ? "Sign in with google" : "Sign up with google"}
+            <Button type="button" color="#f3f3f3" style={{ marginTop: '0.5rem' }} fontColor="#121212">
+              {mode === "login" ? (
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem" }}>
+                  Sign in with Google
+                </div>
+              ) : (
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem" }}>
+                  <img src={GoogleIcon} alt="Google" width="30px" height="30px" />
+                  Sign up with Google
+                </div>
+              )}
             </Button>
             <p style={{textAlign: "center", fontSize: ".9rem", marginTop: ".3rem"}}> 
               {mode === "login" && ( 
@@ -73,9 +134,7 @@ const AuthForm = ({ mode }: AuthFormProps) => {
             {error && <ErrorText>{error}</ErrorText>}
           </FormContainer> 
         </LeftContainer>
-        <RightContainer>
-          
-        </RightContainer>
+        <RightContainer> </RightContainer>
       </>
   );
 };
