1cd96428fcd874e7c0cc5aed806920f319ea6060
Perktus <mefixek2019@gmail.com>
2025-11-16 17:29:12 +0100

    fix: add missing exports

--- project/frontend/src/components/index.ts
+++ project/frontend/src/components/index.ts
@@ -3,6 +3,8 @@ export { default as RightSidebar } from './layout/RightSidebar/RightSidebar';
 export { default as CanvasArea } from './layout/CanvasArea/CanvasArea';
 export { default as DeviceTool } from './layout/DeviceTool/DeviceTool';
 
+export { default as Area } from './simulation/Area/Area'
+
 export { default as DeviceNode } from './editor/DeviceNode/DeviceNode';
 export { default as GroupNode } from './editor/ContextGroupMenu/ContextGroupMenu';
 export { default as GlobalContextMenu } from './editor/GlobalContextMenu/GlobalContextMenu';
@@ -17,3 +19,5 @@ export { default as PortLinkMenu } from './editor/PortLinkMenu/PortLinkMenu'
 export { default as AuthForm } from './auth/AuthForm/AuthForm';
 export { default as AuthLayout } from './auth/AuthLayout/AuthLayout';
 export { default as ProtectedRoute } from './common/ProtectedRoute/ProtectedRoute';
+
+export { default as Modal } from './common/Modal/Modal'
