599d2e64de7cad33878096beb5311cd86aa7a0e6
Perktus <mefixek2019@gmail.com>
2025-11-29 13:34:43 +0100

    chore: remove useFetch, useAuth, useMutation exports

--- project/frontend/src/hooks/index.ts
+++ project/frontend/src/hooks/index.ts
@@ -1,9 +1,5 @@
-export { default as useFetch } from './useFetch/useFetch';
-export { default as useMutation } from './useMutation/useMutation';
-export { default as useAuth } from './useAuth/useAuth';
 export { default as useKeyboardShortcuts } from './useKeyboardShortcuts/useKeyboardShortcuts'
 export { default as useWebSocket } from './useWebSocket/useWebSocket'
-
 export { default as useAuthQuery } from './useAuthQuery/useAuthQuery'
 export { default as useLoginMutation } from './useLoginMutation/useLoginMutation'
 export { default as useRegisterMutation } from './useRegisterMutation/useRegisterMutation'
